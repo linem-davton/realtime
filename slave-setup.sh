24decf0fe5f90f6ec56cc9ca885f6e6964e82cc1
Linem Davton <linemdavton@gmail.com>
2024-05-08 10:46:35 +0200

    made npt2ptp.sh executable in setup script

--- slave-setup.sh
+++ slave-setup.sh
@@ -3,10 +3,13 @@ sudo apt install linuxptp
 
 sudo timedatectl set-ntp 0 # disable ntp
 
-sudo cp systemdUnits/ptp-slave.service /etc/systemd/system/ptp-slave.service
-sudo cp systemdUnits/phc2sys-slave.service /etc/systemd/system/phc2sys-slave.service
-sudo cp systemdUnits/ntp2ptp.service /etc/systemd/system/ntp2ptp.service
-sudo cp systemdUnits/ntp2ptp.sh /usr/local/bin/ntp2ptp.sh
+sudo cp -v systemdUnits/ptp-slave.service /etc/systemd/system/ptp-slave.service
+sudo cp -v systemdUnits/phc2sys-slave.service /etc/systemd/system/phc2sys-slave.service
+sudo cp -v systemdUnits/ntp2ptp.service /etc/systemd/system/ntp2ptp.service
+sudo cp -v systemdUnits/ntp2ptp.sh /usr/local/bin/ntp2ptp.sh
+
+# Make the scripts executable
+sudo chmod -v +x /usr/local/bin/ntp_to_ptp_switch.sh
 
 # Starts ntp server at boot and then switches to ptp
 sudo systemctl enable ntp2ptp.service
